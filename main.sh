635d1bed3a6f17fd88a75a2a48cedc9123dc0834
Толстой Егор <igrekde@gmail.com>
2018-12-23 00:46:17 +0300

    Added draft main.sh

---
+++ main.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# track_id
+echo "SoundCloud track ID: $1"
+
+# ad file name
+echo "Adv filename: $2"
+
+# ad timestamp
+echo "Adv timestamp: $3s"
+
+# ad paid count
+echo "Adv paid count: $4"
+
+##### Downloading and saving track
+
+
+##### Integrating adv
+
+
+##### Getting current play count
+
+
+##### Uploading track with ad
+
+
+##### Polling statistics
+
+
+##### Uploading original track
+
