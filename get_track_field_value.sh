4e58f96b5eba06df63126df342b8adce0f54041f
Tsyganov Stanislav <alloy.stas@gmail.com>
2018-12-23 01:53:35 +0300

    get_playback_count => get_track_field_value

--- get_track_field_value.sh
+++ get_track_field_value.sh
@@ -2,6 +2,7 @@
 
 track_id=$1
 token=$2
+field_name=$3
 
 if [ -z $track_id ]
 then
@@ -15,7 +16,13 @@ then
     exit;
 fi
 
-result=`curl --request GET --url "https://api.soundcloud.com/me/tracks/$track_id?oauth_token=$token" | jq {playback_count}`
+if [ -z $field_name ]
+then
+    echo 'pass field name as third argument'
+    exit;
+fi
+
+result=`curl --request GET --url "https://api.soundcloud.com/me/tracks/$track_id?oauth_token=$token" | jq {$field_name}`
 playback_count=(`echo $result | tr '""' ' '`)
 
 echo ${playback_count[3]}
